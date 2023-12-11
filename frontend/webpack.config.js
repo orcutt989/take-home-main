92f4444eaee149dded64cdc82c5f8fd9e86a8a1c
orcutt989 <orcutt989@gmail.com>
2023-12-11 11:35:22 -0500

    try new path

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     plugins: [PnpWebpackPlugin.moduleLoader(module)],
   },
   output: {
-    path: buildDir,
+    path: '/',
     filename: "[name].bundle.js",
   },
   module: {
